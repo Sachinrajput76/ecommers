f86f63cd12686a13077b240a08047cfa38dfc22d
Sachin rajput <sachinrajputlive@gmail.com>
2021-06-30 16:00:27 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/header'
 import Footer from './components/footer'
 import Homescrean from './screens/Homescreen'
 import Productscreen from './screens/Productscreen'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Page404 from './Page404'
 
 const App = () => {
@@ -13,9 +13,11 @@ const App = () => {
       <Header />
       <main>
         <Container>
-          <Route exact path='ecommers/' component={Homescrean} />
-          <Route path='ecommers/Product/:id' component={Productscreen} />
-          <Route path='*' component={Page404} />
+          <Switch>
+            <Route exact path='/' component={Homescrean} />
+            <Route path='Product/:id' component={Productscreen} />
+            <Route path='*' component={Page404} />
+          </Switch>
         </Container>
       </main>
       <Footer />
